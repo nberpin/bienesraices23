2ad0a95801a9d071430b1fb831daa93388b665c9
nberpin <116831696+nberpin@users.noreply.github.com>
2023-09-26 10:27:59 +0100

    creando función para templates

---
+++ includes/app.php
@@ -0,0 +1,5 @@
+<?php 
+    define('TEMPLATES_URL',__DIR__.'\\templates');
+    define('FUNCIONES_URL', __DIR__.'funciones.php');
+
+    //es conveniente no cerrar el php en los archivos que se van a incluir. 
