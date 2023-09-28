e7fb9be8f1f9c275b4db5ce5c5c29d4dcc2aebfd
nberpin <116831696+nberpin@users.noreply.github.com>
2023-09-28 02:10:47 +0100

    get y post

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,40 +1,50 @@
 <?php 
+//Base de datos
+    require '../../includes/config/database.php';
+    $db= conectarDB();
+    if ($_SERVER['REQUEST_METHOD']==='POST'){
+        
+        echo "<pre>";
+        var_dump($_SERVER);
+        echo "</pre>";
+    }
     require '../../includes/funciones.php';
     incluirTemplate('header');
+    
 
 ?>
 
 <main class="contenedor seccion">
     <h1>Crear</h1>
     <a href="/admin/" class="boton boton-verde">Volver</a>
-    <form class="formulario" action="">
+    <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
         <fieldset>
             <legend> Información General</legend>
 
             <label for="titulo">Título: </label>
-            <input type="text" id="titulo" placeholder="Título propiedad">
+            <input type="text" id="titulo" name="titulo" placeholder="Título propiedad">
             
             <label for="precio">Precio: </label>
-            <input type="number" id="precio" placeholder="Precio propiedad">
+            <input type="number" id="precio" name="precio" placeholder="Precio propiedad">
 
             <label for="imagen">Imagen: </label>
-            <input type="file" id="imagen" accept="image/jpeg, image/png">
+            <input type="file" id="imagen" name="imagen" accept="image/jpeg, image/png">
 
             <label for=" descripcion"> Descripción: </label>
-            <textarea id="descripcion" cols="30" rows="10"></textarea>
+            <textarea id="descripcion" name="descripcion" cols="30" rows="10"></textarea>
 
         </fieldset>
         <fieldset>
             <legend> Información de la propiedad</legend>
 
             <label for="habitaciones">Título: </label>
-            <input type="number" id="habitaciones"  placeholder="Ej.3">
+            <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej.3">
 
             <label for="wc">Baños: </label>
-            <input type="number" id="wc" placeholder="Ej.1">
+            <input type="number" id="wc" name="wc" placeholder="Ej.1">
 
             <label for="estacionamiento">Baños: </label>
-            <input type="number" id="estacionamiento" placeholder="Ej.1">
+            <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej.1">
 
         </fieldset>
 
