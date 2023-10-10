11d9ad464cad3a34e2b56176e2076db5e01f5f78
nberpin <116831696+nberpin@users.noreply.github.com>
2023-10-10 15:49:01 +0100

    mostrar propiedades y alertas!

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <?php 
-    $resultado=$_GET['resultado'];
+    $resultado=$_GET['resultado']??null;
     require '../includes/funciones.php';
     incluirTemplate('header');
 
@@ -11,6 +11,31 @@
         <p class="alerta exito">Anuncio creado correctamente</p>
     <?php } ?>
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                    <a class="boton-rojo-block" href="#">Eliminar</a>
+                    <a class="boton-verde-block" href="#">Actualizar</a>
+                </td>
+            </tr>
+        </tbody>
+
+    </table>
     
 </main>
 
